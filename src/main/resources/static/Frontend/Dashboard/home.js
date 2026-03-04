0b625086defa0673f1e09040028f9a0ec52517e8
Atharvsurya <atharvsurya007@gmail.com>
2026-03-04 17:24:00 +0530

    ?

--- src/main/resources/static/Frontend/Dashboard/home.js
+++ src/main/resources/static/Frontend/Dashboard/home.js
@@ -1,26 +1,3 @@
-// const scrollContainer = document.getElementById("courses");
-
-//     let scrollAmount = 0;
-//     const scrollStep = 600;
-//     const delay = 3000;
-
-//     setInterval(() => {
-//         scrollAmount += scrollStep;
-//         if (scrollAmount >= scrollContainer.scrollWidth - scrollContainer.clientWidth) {
-//             scrollAmount = 0;
-//         }
-
-//         scrollContainer.scrollTo({
-//             left: scrollAmount,
-//             behavior: "smooth"
-//         });
-//     }, delay);
-
-const btn = document.getElementById("scroll");
-
-  btn.addEventListener("click", function () {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  });
+function topfun(){
+  document.documentElement.scrollTop = 0;
+}
